55417497d0290b2e77595a73f8e4f76ccf291ffb
DEVUCP <OMHN05@protonmail.com>
2025-05-10 16:29:05 +0300

    feat(program): Add filepath argument parsing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using FileHandling;
 
-
-using var image = Image.Load<Rgba32>("resources/aluber.png");
-var resizer = new ImageProcessing.SeamCarver();
-resizer.ProcessImage(image, new ImageProcessing.ImageSize(520, 520));
-image.Save("result.png");
+if (args.Length > 0)
+{
+    if (!FileUtils.checkFileExists(args[0]))
+    {
+        Console.WriteLine("File does not exist or Incorrect file path.");
+        return;
+    }
+    string inputPath = args[0];
+    using var image = Image.Load<Rgba32>(inputPath);
+    var resizer = new ImageProcessing.SeamCarver();
+    resizer.ProcessImage(image, new ImageProcessing.ImageSize(520, 520));
+    image.Save("result.png");
+}
+else
+{
+    Console.WriteLine("Please provide an input image path as argument.");
+    Console.WriteLine("Usage: resize-tool.exe <input-image-path>");
+}
